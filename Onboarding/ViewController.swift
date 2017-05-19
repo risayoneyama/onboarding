2f4704d689dde2ae6dcc40d7a94136d9c5c5d25a
Risa Yoneyama <risa@modiface.com>
2017-05-19 11:33:07 -0400

    Initial Commit

---
+++ Onboarding/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Onboarding
+//
+//  Created by Risa Yoneyama on 2017-05-19.
+//  Copyright © 2017 Risa Yoneyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
